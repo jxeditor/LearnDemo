a421c0d54b29c7726477a1b32d430743561b0754
jxeditor <980813351@qq.com>
2021-10-18 10:10:16 +0800

    刷题刷题

--- Algorithm/src/main/java/org/example/leetcode/q0001t0100/q2/Question0019.java
+++ Algorithm/src/main/java/org/example/leetcode/q0001t0100/q2/Question0019.java
@@ -1,6 +1,22 @@
-package org.example.leetcode.q0001t0100.q2;/**
+package org.example.leetcode.q0001t0100.q2;
+
+/**
  * @Author xz
  * @Date 2021/10/18 08:44
  * @Description TODO
- */public class Question0019 {
+ */
+public class Question0019 {
+    static class ListNode {
+        int val;
+
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    }
 }
