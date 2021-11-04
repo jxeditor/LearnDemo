4a6483b64d392910f5f6b15c324a7d2e4dd03533
jxeditor <980813351@qq.com>
2021-11-04 11:26:11 +0800

    刷题刷题

--- Algorithm/src/main/java/org/example/leetcode/q0001t0100/q5/Question0044.java
+++ Algorithm/src/main/java/org/example/leetcode/q0001t0100/q5/Question0044.java
@@ -55,4 +55,78 @@ package org.example.leetcode.q0001t0100.q5;
  * 动态规划
  */
 public class Question0044 {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        // 建立m+1,n+1矩阵,例如abcd与*d,则是5*3矩阵
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        // 初始化好第一层矩阵值,都符合*,所以是T,T,T
+        for (int i = 1; i <= n; ++i) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') { // 如果通配符为*,选取矩阵位置上一个结果以及矩阵位置左侧值
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                } else if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)) {// 如果为?或字符串相等则取前一个匹配字符串的符合结果
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+
+    // 贪心
+    public boolean isMatch2(String s, String p) {
+        int sRight = s.length(), pRight = p.length();
+        while (sRight > 0 && pRight > 0 && p.charAt(pRight - 1) != '*') {
+            if (charMatch(s.charAt(sRight - 1), p.charAt(pRight - 1))) {
+                --sRight;
+                --pRight;
+            } else {
+                return false;
+            }
+        }
+        if (pRight == 0) {
+            return sRight == 0;
+        }
+        int sIndex = 0, pIndex = 0;
+        int sRecord = -1, pRecord = -1;
+        while (sIndex < sRight && pIndex < pRight) {
+            if (p.charAt(pIndex) == '*') {
+                ++pIndex;
+                sRecord = sIndex;
+                pRecord = pIndex;
+            } else if (charMatch(s.charAt(sIndex), p.charAt(pIndex))) {
+                ++sIndex;
+                ++pIndex;
+            } else if (sRecord != -1 && sRecord + 1 < sRight) {
+                ++sRecord;
+                sIndex = sRecord;
+                pIndex = pRecord;
+            } else {
+                return false;
+            }
+        }
+        return allStars(p, pIndex, pRight);
+    }
+
+    public boolean allStars(String str, int left, int right) {
+        for (int i = left; i < right; ++i) {
+            if (str.charAt(i) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean charMatch(char u, char v) {
+        return u == v || v == '?';
+    }
 }
