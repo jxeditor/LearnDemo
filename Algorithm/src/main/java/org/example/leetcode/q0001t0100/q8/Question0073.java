b7720e17abc3484cc387d7589c6ce060d3eab378
jxeditor <980813351@qq.com>
2021-11-09 17:14:33 +0800

    刷题刷题

--- Algorithm/src/main/java/org/example/leetcode/q0001t0100/q8/Question0073.java
+++ Algorithm/src/main/java/org/example/leetcode/q0001t0100/q8/Question0073.java
@@ -1,5 +1,7 @@
 package org.example.leetcode.q0001t0100.q8;
 
+import java.util.Arrays;
+
 /**
  * @Author xz
  * @Date 2021/11/9 15:10
@@ -33,5 +35,32 @@ package org.example.leetcode.q0001t0100.q8;
  * 矩阵
  */
 public class Question0073 {
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
+        // 记录行列标识是否含有0
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || col[j] == 1) matrix[i][j] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] inputs = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        setZeroes(inputs);
+        System.out.println(Arrays.deepToString(inputs));
+    }
 }
