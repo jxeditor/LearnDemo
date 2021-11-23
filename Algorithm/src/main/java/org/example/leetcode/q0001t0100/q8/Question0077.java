0b344ea9a4a831c3c3b96840b9c1ee082ba88d41
jxeditor <980813351@qq.com>
2021-11-23 09:05:12 +0800

    刷题刷题

---
+++ Algorithm/src/main/java/org/example/leetcode/q0001t0100/q8/Question0077.java
@@ -0,0 +1,66 @@
+package org.example.leetcode.q0001t0100.q8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author xz
+ * @Date 2021/11/23 08:40
+ * @Description TODO
+ * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+ * <p>
+ * 你可以按 任何顺序 返回答案。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 4, k = 2
+ * 输出：
+ * [
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
+ * ]
+ * 示例 2：
+ * <p>
+ * 输入：n = 1, k = 1
+ * 输出：[[1]]
+ * 提示：
+ * <p>
+ * 1 <= n <= 20
+ * 1 <= k <= n
+ * Related Topics
+ * 数组
+ * 回溯
+ */
+class Question0077 {
+    List<Integer> temp = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        dfs(1, n, k);
+        return ans;
+    }
+
+    public void dfs(int cur, int n, int k) {
+        // 剪枝：temp 长度加上区间 [cur, n] 的长度小于 k，不可能构造出长度为 k 的 temp
+        // 当某一个位置时必定满足之后的元素拼凑不了temp
+        if (temp.size() + (n - cur + 1) < k) {
+            return;
+        }
+        // 记录合法的答案
+        if (temp.size() == k) {
+            ans.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        // 考虑选择当前位置
+        temp.add(cur);
+        dfs(cur + 1, n, k);
+        temp.remove(temp.size() - 1);
+        // 考虑不选择当前位置
+        dfs(cur + 1, n, k);
+    }
+}
+
---
+++ Algorithm/src/main/java/org/example/leetcode/q0001t0100/q8/Question0078.java
@@ -0,0 +1,51 @@
+package org.example.leetcode.q0001t0100.q8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author xz
+ * @Date 2021/11/23 08:47
+ * @Description TODO
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * <p>
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ * nums 中的所有元素 互不相同
+ * Related Topics
+ * 位运算
+ * 数组
+ * 回溯
+ */
+public class Question0078 {
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<Integer>(t));
+            return;
+        }
+        t.add(nums[cur]);
+        dfs(cur + 1, nums);
+        t.remove(t.size() - 1);
+        dfs(cur + 1, nums);
+    }
+}
