0ccce03d03a35bd5b0081bb1d8ef8b5401831edc
jxeditor <980813351@qq.com>
2021-11-23 16:13:39 +0800

    刷题刷题

---
+++ Algorithm/src/main/java/org/example/leetcode/q0001t0100/q8/Question0080.java
@@ -0,0 +1,62 @@
+package org.example.leetcode.q0001t0100.q8;
+
+/**
+ * @Author xz
+ * @Date 2021/11/23 14:58
+ * @Description TODO
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * 说明：
+ * <p>
+ * 为什么返回数值是整数，但输出的答案是数组呢？
+ * <p>
+ * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ * <p>
+ * 你可以想象内部操作如下:
+ * <p>
+ * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+ * int len = removeDuplicates(nums);
+ * <p>
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,1,2,2,3]
+ * 输出：5, nums = [1,1,2,2,3]
+ * 解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。 不需要考虑数组中超出新长度后面的元素。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,0,1,1,1,1,2,3,3]
+ * 输出：7, nums = [0,0,1,1,2,3,3]
+ * 解释：函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。 不需要考虑数组中超出新长度后面的元素。
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 3 * 104
+ * -104 <= nums[i] <= 104
+ * nums 已按升序排列
+ * Related Topics
+ * 数组
+ * 双指针
+ */
+public class Question0080 {
+    // 对比第26题,相同元素最多可以出现2次
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+        // 修改下比较的间隔,从原来的向前比较,改为上上个比较
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+}
