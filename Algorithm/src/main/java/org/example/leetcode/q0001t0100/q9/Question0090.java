4c4d3504984eee7ff0ffeadc8c8afb34ccc21a9e
jxeditor <980813351@qq.com>
2021-11-29 14:33:47 +0800

    UpsertStreamTableSink中DDL与外部数据结构关系

---
+++ Algorithm/src/main/java/org/example/leetcode/q0001t0100/q9/Question0090.java
@@ -0,0 +1,57 @@
+package org.example.leetcode.q0001t0100.q9;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author xz
+ * @Date 2021/11/29 14:22
+ * @Description TODO
+ * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+ * <p>
+ * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,2]
+ * 输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ * Related Topics
+ * 位运算
+ * 数组
+ * 回溯
+ */
+public class Question0090 {
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            boolean flag = true;
+            for (int i = 0; i < n; ++i) {
+                if ((mask & (1 << i)) != 0) {
+                    if (i > 0 && (mask >> (i - 1) & 1) == 0 && nums[i] == nums[i - 1]) {
+                        flag = false;
+                        break;
+                    }
+                    t.add(nums[i]);
+                }
+            }
+            if (flag) {
+                ans.add(new ArrayList<Integer>(t));
+            }
+        }
+        return ans;
+    }
+}
