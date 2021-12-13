27e653db410fe55fe658b99bae641123aadf4895
jxeditor <980813351@qq.com>
2021-12-13 10:04:23 +0800

    解题

---
+++ Algorithm/src/main/java/org/example/leetcode/q0101t0200/q1/Question0102.java
@@ -0,0 +1,78 @@
+package org.example.leetcode.q0101t0200.q1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Author xz
+ * @Date 2021/12/13 09:57
+ * @Description TODO
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * <p>
+ * 示例：
+ * 二叉树：[3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其层序遍历结果：
+ * <p>
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ * Related Topics
+ * 树
+ * 广度优先搜索
+ * 二叉树
+ */
+public class Question0102 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int currentLevelSize = queue.size();
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ret.add(level);
+        }
+        return ret;
+    }
+}
